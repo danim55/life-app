090a3d1fb8c502bd84ff1e61d555beae5d29001c
danim55 <dmerinocano@gmail.com>
2023-01-19 19:37:07 +0100

    First Version of the app

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe','This is simply a test','https://freesvg.org/img/RecipeBook.png'),
+    new Recipe('Another test Recipe','This is also a test','https://freesvg.org/img/RecipeBook.png'),
+  ];
+
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+
+}
