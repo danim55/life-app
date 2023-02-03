e0720dc3d58dcc6a9b5ae922acf22d77533db183
danim55 <dmerinocano@gmail.com>
2023-02-03 20:41:59 +0100

    Add ingredients to recipe.model and display them

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,21 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test Recipe', 'This is simply a test', 'https://freesvg.org/img/RecipeBook.png'),
-        new Recipe('Another test Recipe', 'This is also a test', 'https://freesvg.org/img/RecipeBook.png'),
+        new Recipe('Delicious Burguer',
+        'Some classic burguer', 
+        'https://c.pxhere.com/images/c8/c8/652bd1231b43ef2e00372ed23e45-1636310.jpg!d',
+        [
+            new Ingredient('Cheese',2),
+            new Ingredient('Meat',1)
+        ]),
+        new Recipe('Pizza',
+        'A carbonara pizza', 
+        'https://www.publicdomainpictures.net/pictures/40000/velka/fresh-pizza.jpg',
+        [
+            new Ingredient('Cheese',2),
+            new Ingredient('Tomato',1),
+            new Ingredient('Bread',3)
+        ]),
     ];
 
     getRecipes(){
