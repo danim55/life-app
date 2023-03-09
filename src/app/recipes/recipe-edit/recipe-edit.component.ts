825b98cf666f804da467a86046f13f95e72be73e
danim55 <dmerinocano@gmail.com>
2023-03-09 16:05:47 +0100

    Functionality for delete, save and cancel buttons

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -15,7 +15,10 @@ export class RecipeEditComponent implements OnInit {
   editMode: boolean = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService,
+    private router: Router) { }
 
   ngOnInit() {
     this.route.params
@@ -29,17 +32,15 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    const recipe: Recipe = new Recipe(
-      this.recipeForm.value.name,
-      this.recipeForm.value.imagePath,
-      this.recipeForm.value.description,
-      this.recipeForm.value.ingredients,
-    )
     if (this.editMode) {
       this.recipeService.updateRecipe(this.id, this.recipeForm.value)
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+  }
+  
+  onCancel(){
+    this.router.navigate([''], { relativeTo: this.route })
   }
 
   get controls() { // a getter!
