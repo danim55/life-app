eb175bbd06ccb31cf9f24790aba9463679728136
danim55 <dmerinocano@gmail.com>
2023-04-05 13:53:47 +0200

    Calling put endpoint of firebase to store recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,23 @@
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClientModule){}
+    constructor(
+        private http: HttpClient,
+        private recipeService: RecipeService) { }
+
+    storeRecipes() {
+        const recipes: Recipe[] = this.recipeService.getRecipes();
+        this.http
+            .put(
+                'https://life-app-add1c-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+                recipes
+            )
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 }
