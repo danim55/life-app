1dd3028d65a35f563503e6fe26193073ceffa2a4
danim55 <dmerinocano@gmail.com>
2023-04-15 20:56:44 +0200

    Attaching the Token with an Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -25,28 +25,23 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1),
-            exhaustMap(user => {
-                return this.http.get<Recipe[]>(
-                        'https://life-app-add1c-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-                        {
-                            params: new HttpParams().set('auth', user.token)
-                        }
-                    )
-            }),
+
+        return this.http.get<Recipe[]>(
+            'https://life-app-add1c-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        )
+        .pipe(
 
             map(recipes => {
                 return recipes.map(recipe => {
                     return {
                         ...recipe,
                         ingredients: recipe.ingredients ? recipe.ingredients : []
-                    }
-                })
+                    };
+                });
             }),
             tap(recipes => {
                 this.recipeService.setRecipes(recipes);
             })
-        )
+        );
     }
 }
