b302073b2b4cab422ecf883b6a758695ec68bb73
danim55 <dmerinocano@gmail.com>
2023-04-26 11:22:44 +0200

    Implementing lazy loading

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeStartComponent } from "../recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { ReactiveFormsModule } from "@angular/forms";
-import { AppRoutingModule } from "../app-routing.module";
-import { CommonModule } from "@angular/common";
 import { RecipesRouterModule } from "./recipes-router.module";
-import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -21,7 +21,7 @@ import { SharedModule } from "../shared/shared.module";
         RecipeEditComponent,
     ],
     imports: [
-        AppRoutingModule,
+        RouterModule,
         ReactiveFormsModule,
         SharedModule,
         RecipesRouterModule,
