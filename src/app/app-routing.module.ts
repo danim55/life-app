c8fc5c1c5d631be9f262a816f0684e77740b269c
danim55 <dmerinocano@gmail.com>
2023-04-26 11:32:02 +0200

    ShoppingList lazy module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from "@angular/router";
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(mod => mod.RecipesModule) },
+    { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(mod => mod.ShoppingListModule) },
 ]
 
 @NgModule({
