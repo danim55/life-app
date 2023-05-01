863a02f31a12fd311c4f4dc76e924292a92b36a3
danim55 <dmerinocano@gmail.com>
2023-05-01 16:10:51 +0200

    Adding apiFirebaseKey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseApiKey: 'AIzaSyCo24EenKtbw6UJ9hOllV1IOR_dd3fC6FM'
 };
 
 /*
